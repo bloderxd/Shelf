bddb669a7ef039b097ff96c0a8b280993861ee2c
Daniel <wowmegadenis@hotmail.com>
2017-01-31 00:26:30 -0200

    Add shelf call example in test main activity

--- app/src/main/java/com/bloder/shelf/MainActivity.java
+++ app/src/main/java/com/bloder/shelf/MainActivity.java
@@ -3,11 +3,14 @@ package com.bloder.shelf;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import bloder.Shelf;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Shelf.forTest().inProdEnvironment().test();
     }
 }
