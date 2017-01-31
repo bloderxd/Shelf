e85fcbcb7a92d0e8781121987564b1f2b05e585d
Daniel <wowmegadenis@hotmail.com>
2017-01-31 00:28:52 -0200

    Create test locker fake implementation

---
+++ app/src/main/java/com/bloder/shelf/test/TestFake.java
@@ -0,0 +1,16 @@
+package com.bloder.shelf.test;
+
+import com.example.Fake;
+
+/**
+ * Created by bloder on 31/01/17.
+ */
+
+@Fake
+public class TestFake implements Test {
+
+    @Override
+    public void test() {
+
+    }
+}
