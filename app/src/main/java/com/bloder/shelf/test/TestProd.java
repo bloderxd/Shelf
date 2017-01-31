e658a70c1b88f686157f2031c6ac1220c065c388
Daniel <wowmegadenis@hotmail.com>
2017-01-31 00:29:05 -0200

    Create test locker prod implementation

---
+++ app/src/main/java/com/bloder/shelf/test/TestProd.java
@@ -0,0 +1,16 @@
+package com.bloder.shelf.test;
+
+import com.example.Prod;
+
+/**
+ * Created by bloder on 31/01/17.
+ */
+
+@Prod
+public class TestProd implements Test {
+
+    @Override
+    public void test() {
+
+    }
+}
