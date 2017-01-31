02ce49e75a63c0528990c29c66417aaeb22bb899
Daniel <wowmegadenis@hotmail.com>
2017-01-30 23:55:34 -0200

    Create prod annotation

---
+++ core/src/main/java/com/example/Prod.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by bloder on 30/01/17.
+ */
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.TYPE)
+public @interface Prod {
+}
